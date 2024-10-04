fd5d6652d3e64f9eeac106aa6fb33575c911ea2a
daindaind <cdin23@naver.com>
2024-10-05 01:13:17 +0900

    Week 7: 최대값과 최소값 솔루션

--- week7/최대값과 최소값.js
+++ week7/최대값과 최소값.js
@@ -1,25 +1,28 @@
 function solution(s) {
   const list = s.split(" ");
-  let minusList = [];
-  let plusList = [];
+  let minus = [];
+  let plus = [];
 
-  list.map((item, index) => {
+  list.forEach((item) => {
     if (item.includes("-")) {
-      minusList.push(item);
+      minus.push(item);
     } else {
-      plusList.push(item);
+      plus.push(item);
     }
   });
 
-  plusList = plusList.sort(function (a, b) {
-    if (parseInt(a) > parseInt(b)) return 1;
-    if (parseInt(a) === parseInt(b)) return 0;
-    if (parseInt(a) < parseInt(b)) return -1;
-  });
+  // 절대값이 작은 수부터 정렬
+  // 양수 리스트 result: ['1', '2', '3']
+  const sortedPlus = plus.sort((a, b) => parseInt(a) - parseInt(b));
 
-  minusList = minusList.sort(function (a, b) {
-    if (parseInt(a) > parseInt(b)) return 1;
-    if (parseInt(a) === parseInt(b)) return 0;
-    if (parseInt(a) < parseInt(b)) return -1;
-  });
+  // 음수 리스트 result: ['-4', '-3', '-2', '-1']
+  const sortedMinus = minus.sort(
+    (a, b) =>
+      parseInt(b.slice(1, b.length).toString()) -
+      parseInt(a.slice(1, a.length).toString())
+  );
+
+  // 두 리스트를 합침!
+  const joinedList = [...sortedMinus, ...sortedPlus];
+  return [joinedList[0], joinedList[joinedList.length - 1]].join(" ");
 }
