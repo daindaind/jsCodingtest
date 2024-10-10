153e9819a7f1e9e968283fecc9bf148dc0559930
daindaind <cdin23@naver.com>
2024-10-10 12:46:48 +0900

    Week7 이진 변환 반복하기 솔루션 다시 풀이

--- week7/이진 변환 반복하기.js
+++ week7/이진 변환 반복하기.js
@@ -1,11 +1,26 @@
 function solution(s) {
+  let result = s;
   let count = 0;
+  let turnCount = 0;
 
-  [...s].forEach((item, idx) => {
-    if (item === "1") {
-      count += 1;
-    }
-  });
+  const deleteZero = (num) => {
+    let count = 0;
 
-  // 이진 변환 로직 구현
+    [...num].forEach((item, idx) => {
+      if (item === "1") {
+        count += 1;
+      } else {
+        turnCount += 1;
+      }
+    });
+
+    return count;
+  };
+
+  while (result !== "1") {
+    result = deleteZero(result).toString(2);
+    count++;
+  }
+
+  return [count, turnCount];
 }
