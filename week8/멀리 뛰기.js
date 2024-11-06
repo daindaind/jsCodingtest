480cee3a8df92e1ce1a2c3957cce92c91ec642f5
daindaind <cdin23@naver.com>
2024-11-06 11:31:25 +0900

    Week8 멀리 뛰기 풀이

--- week8/멀리 뛰기.js
+++ week8/멀리 뛰기.js
@@ -1,8 +1,16 @@
 function solution(n) {
   function fibo(num) {
-    return num >= n ? fibo(num - 1) + fibo(num - 2) : num;
+    if (num === 1) return 1;
+
+    let stack = [1, 1];
+    for (let i = 2; i < n + 1; i++) {
+      let number = (stack[i - 1] % 1234567) + (stack[i - 2] % 1234567);
+      stack.push(number % 1234567);
+    }
+
+    return stack[num] % 1234567;
   }
 
   const result = fibo(n);
-  return result;
+  return result % 1234567;
 }
